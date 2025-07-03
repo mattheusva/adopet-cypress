784d4a714b34a42ffcfd273b0ad13a94fb822eda
matheus <matheusvargas042@gmail.com>
2025-07-03 01:15:16 -0300

    02-automatizando cenarios de teste

---
+++ cypress/e2e/cadastro-correto.cy.js
@@ -0,0 +1,13 @@
+describe('Página de cadastro', () => {
+  beforeEach(() => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('[data-test="register-button"]').click();
+  })
+  it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
+    cy.get('[data-test="input-name"]').type('Cel 123');
+    cy.get('[data-test="input-email"]').type('cel123@email.com');
+    cy.get('[data-test="input-password"]').type('Senha123');
+    cy.get('[data-test="input-confirm-password"]').type('Senha123');
+    cy.get('[data-test="submit-button"]').click();
+  })
+})
