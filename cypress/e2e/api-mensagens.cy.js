cc55db9afecb7c06b581a094d7c3bda6d9092da8
matheus <matheusvargas042@gmail.com>
2025-07-04 15:34:54 -0300

    05-Protegendo dados sensiveis

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,11 +1,13 @@
 describe('Api Adopet', () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTc1MTU4NjMxOCwiZXhwIjoxNzUxODQ1NTE4fQ.XnhdAP2T3Ws7lFNGqyhjZlgAHUWiz1C8yyiSNTKzy4Y`
+    
+    //Atenção, o token possui um prazo de validade. Dessa forma, você deverá realizar o login e copiar o toker presente no Local Storage do seu navegador.
+    //Você encontrará um cypress.en.json_exemplo para entender como o token deve ser inserido no arquivo cypress.env.json
 
     it('Mensagens da API', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
-            headers: { authorization }
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
