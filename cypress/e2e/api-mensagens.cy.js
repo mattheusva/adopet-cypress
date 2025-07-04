11123370ce6f5876895f160c4b87759c20fae6b0
matheus <matheusvargas042@gmail.com>
2025-07-03 21:27:43 -0300

    04-utilizando stubs e fixtures

---
+++ cypress/e2e/api-mensagens.cy.js
@@ -0,0 +1,15 @@
+describe('Api Adopet', () => {
+    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTc1MTU4NjMxOCwiZXhwIjoxNzUxODQ1NTE4fQ.XnhdAP2T3Ws7lFNGqyhjZlgAHUWiz1C8yyiSNTKzy4Y`
+
+    it('Mensagens da API', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
+            headers: { authorization }
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('msg')
+        })
+    })
+})
